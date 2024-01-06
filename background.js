7e4c44f048ea69f4bfe48d0613f0d579179b443f
naiba <hi@nai.ba>
2024-01-06 19:00:15 +0800

    Update extension status checking logic

--- background.js
+++ background.js
@@ -2,6 +2,34 @@
 
 import { setBadgeAndShield } from "./utils.js";
 
+async function getExtensionStatus(extensionId) {
+    extensionId += '?&' + Date.now()
+    try {
+        const statusCode = await new Promise((resolve, reject) => {
+            fetch("https://corsproxy.io/?https://chrome.google.com/webstore/detail/" + extensionId, {
+                redirect: 'manual'
+            }).then(response => {
+                resolve(response.type == 'opaqueredirect' ? 301 : response.status)
+                return {}
+            }).catch(err => {
+                reject(err)
+            })
+        });
+        return statusCode
+    } catch (error) {
+    }
+    try {
+        const statusCode = await new Promise((resolve, reject) => {
+            fetch("https://api.allorigins.win/get?url=https://chrome.google.com/webstore/detail/" + extensionId, {
+                redirect: 'manual'
+            }).then(resp => resp.json()).then(data => resolve(data.status.http_code == 200 ? 301 : data.status.http_code)).catch(err => reject(err))
+        });
+        return statusCode
+    } catch (error) {
+        throw error
+    }
+}
+
 async function checkExtensions() {
     chrome.management.getAll(async extensions => {
         var totalHighRiskExtensions = 0
@@ -10,23 +38,22 @@ async function checkExtensions() {
             const extension = extensions[i]
             try {
                 // check if extension is from the webstore
-                const statusCode = await new Promise((resolve, reject) => {
-                    fetch("https://corsproxy.io/?https://chrome.google.com/webstore/detail/" + extension.id, {
-                        redirect: 'manual'
-                    }).then(response => {
-                        resolve(response.type == 'opaqueredirect' ? 301 : response.status)
-                        return {}
-                    }).catch(err => {
-                        reject(err)
-                    })
-                });
+                const statusCode = await getExtensionStatus(extension.id)
+                if (statusCode != 301 && statusCode != 404) {
+                    throw "Unexpected status code: " + statusCode
+                }
                 if (statusCode != 301) {
                     totalHighRiskExtensions++
                     highRiskExtensionsReason[extension.id] = "Removed in webstore"
                 }
             } catch (error) {
-                continue;
+                if (typeof error == "object" && 'message' in error) {
+                    error = error.message
+                }
+                totalHighRiskExtensions++
+                highRiskExtensionsReason[extension.id] = "Error checking webstore: " + error
             }
+            await new Promise((resolve, reject) => setTimeout(resolve, Math.random() * 5000 + 5000))
         }
         setBadgeAndShield(totalHighRiskExtensions, false)
         chrome.storage.sync.set({ totalHighRiskExtensions: totalHighRiskExtensions, highRiskExtensionsReason: highRiskExtensionsReason })
